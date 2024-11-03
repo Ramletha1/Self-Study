911c8f466849a0b96b1948169e1685696056b538
Wongsatorn Suwannarit <146055491+Ramletha1@users.noreply.github.com>
2024-11-03 18:56:25 +0000

    /home/codespace

--- c/system_programming/3.multiple_processes/main.c
+++ c/system_programming/3.multiple_processes/main.c
@@ -1,11 +1,30 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
+
+#define TIME 10
 
 void main() {
     pid_t pid = fork();
 
-    if (pid==0) printf("Child Process");
-    else if (pid>1) printf("Parent Process");
+    if (pid==0) {           // Child process
+        printf("Child Process: %d\n", pid);
+        sleep(TIME+1);
+        printf("\nEnd of Child\n");
+        exit(0);
+    }
+
+    else if (pid>1) {       // Parent process
+        printf("Parent Process: %d\n", pid);
+        sleep(1);
+        int i;
+        while (waitpid(NULL, NULL, WNOHANG) == 0) {  // Loop until child process exit
+            printf("%d ", ++i);
+            fflush(stdout);
+            sleep(1);
+        }
+        printf("End of Parent\n");
+    }
 
     return;
 }
