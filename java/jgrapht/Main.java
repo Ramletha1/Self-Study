67ba50a93efcca10cc5e5fc5be2efaeed6ab8814
Wongsatorn Suwannarit <blue25477@gmail.com>
2024-12-10 17:23:56 +0700

    ~

---
+++ java/jgrapht/Main.java
@@ -0,0 +1,9 @@
+import org.jgrapht.graph.*;
+
+public class Main {
+    public static void Main(String args[]) {
+        SimpleGraph<String, DefaultEdge> graph = new SimpleGraph<>(DefaultEdge.class);
+        
+        
+    }
+}
